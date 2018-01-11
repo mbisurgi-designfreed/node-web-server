4248a1c821268a4ebeeb10f04ba7c82d6400f43b
mbisurgi <maximiliano@designfreed.com>
2018-01-11 01:11:04 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    const now = new Date().toString();
+    const log = `${now}: ${req.method} ${req.url}`;
+    
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+
+        next();
+    });
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenence.hbs', {
+//         title: 'Sorry',
+//         welcome: 'Our page is down for the moment. Please try back in a couple of minutes!'
+//     });
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('getUppercase', (text) => text.toUpperCase());
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        title: 'Home Page',
+        welcome: 'Welcome to my new Express App!'
+    })
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        title: 'About Page'
+    });
+});
+
+app.listen(3001, () => {
+    console.log('Server started, listening on port 3000');
+});
